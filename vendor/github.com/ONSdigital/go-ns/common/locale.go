9137cc34e4ed4439ada875213ae7eed40d41f4f5
Daniel W <danieldwalford@gmail.com>
2019-07-25 10:31:19 +0100

    Update common package from go-ns

--- vendor/github.com/ONSdigital/go-ns/common/locale.go
+++ vendor/github.com/ONSdigital/go-ns/common/locale.go
@@ -5,24 +5,25 @@ import (
 	"strings"
 )
 
-// LangEN is a reference to the english short-form localeCode "en"
-const LangEN = "en"
+const (
+	LangEN = "en"
+	LangCY = "cy"
 
-// LangCY is a reference to the english short-form localeCode "cy"
-const LangCY = "cy"
+	DefaultLang = LangEN
 
-// DefaultLang is the desired langauge to default to if requested language is unavailable
-const DefaultLang = LangEN
+	LocaleCookieKey = "lang"
+	LocaleHeaderKey = "LocaleCode"
+)
 
 // SetLocaleCode sets the Locale code used to set the language
 func SetLocaleCode(req *http.Request) *http.Request {
 	localeCode := GetLangFromSubDomain(req)
 
 	// Language is overridden by cookie 'lang' here if present.
-	if c, err := req.Cookie("lang"); err == nil && len(c.Value) > 0 {
+	if c, err := req.Cookie(LocaleCookieKey); err == nil && len(c.Value) > 0 {
 		localeCode = GetLangFromCookieOrDefault(c)
 	}
-	req.Header.Set("LocaleCode", localeCode)
+	req.Header.Set(LocaleHeaderKey, localeCode)
 
 	return req
 }
