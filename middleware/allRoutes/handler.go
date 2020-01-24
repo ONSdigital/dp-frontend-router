a551315892c26fd4a3f8a65441f16eab3f71113a
David Subiros <subiros.david@gmail.com>
2020-01-24 10:32:16 +0000

    migrated from go-ns/log to log.go

--- middleware/allRoutes/handler.go
+++ middleware/allRoutes/handler.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/ONSdigital/dp-frontend-router/config"
 	"github.com/ONSdigital/go-ns/common"
-	"github.com/ONSdigital/go-ns/log"
+	"github.com/ONSdigital/log.go/log"
 )
 
 //Handler ...
@@ -24,14 +24,14 @@ func Handler(routesHandler map[string]http.Handler) func(h http.Handler) http.Ha
 
 			// No point calling zebedee for these paths so skip middleware
 			if ok, err := regexp.MatchString(`^\/(?:datasets|filter|feedback|healthcheck)`, path); ok && err == nil {
-				log.Info("Skipping content specific handling as not relevant on this path.", log.Data{"url": path})
+				log.Event(req.Context(), "Skipping content specific handling as not relevant on this path.", log.Data{"url": path})
 				h.ServeHTTP(w, req)
 				return
 			}
 
 			// We can skip handling based on content type where the url points to a known/expected file extension
 			if ok, err := regexp.MatchString(`^*\.(?:xls|zip|csv|xslx)$`, req.URL.String()); ok && err == nil {
-				log.Info("Skipping content specific handling as it's a request to download a known file extension.", log.Data{"url": req.URL.String()})
+				log.Event(req.Context(), "Skipping content specific handling as it's a request to download a known file extension.", log.Data{"url": req.URL.String()})
 				h.ServeHTTP(w, req)
 				return
 			}
@@ -43,33 +43,33 @@ func Handler(routesHandler map[string]http.Handler) func(h http.Handler) http.Ha
 			} else {
 				contentURL += "?uri=" + path
 			}
-			log.Debug(contentURL, nil)
+			log.Event(req.Context(), "generated from 'collection' cookie", log.Data{"contentURL": contentURL})
 
 			//FIXME We should be doing a HEAD request but Restolino doesn't allow it - either wait for the
 			// new Content API (https://github.com/ONSdigital/dp-content-api) to be in prod or update Restolino
 
 			request, err := http.NewRequest("GET", contentURL, nil)
 			if err != nil {
-				log.ErrorR(req, err, nil)
+				log.Event(req.Context(), "error with GET request", log.Error(err))
 				h.ServeHTTP(w, req)
 				return
 			}
 
 			if c, err := req.Cookie(`access_token`); err == nil && len(c.Value) > 0 {
 				request.Header.Set(`X-Florence-Token`, c.Value)
-				log.Debug(c.Value, nil)
+				log.Event(req.Context(), "Cookie error", log.Data{"value": c.Value})
 			}
 
 			res, err := http.DefaultClient.Do(request)
 			if err != nil {
-				log.ErrorR(req, err, nil)
+				log.Event(req.Context(), "Do request error", log.Error(err))
 				h.ServeHTTP(w, req)
 				return
 			}
 
 			statusCode := res.StatusCode
 			if statusCode >= 400 {
-				log.DebugR(req, "Unexpected status code", log.Data{"statusCode": statusCode, "url": contentURL})
+				log.Event(req.Context(), "Unexpected status code", log.Data{"statusCode": statusCode, "url": contentURL})
 				io.Copy(ioutil.Discard, res.Body)
 				res.Body.Close()
 				h.ServeHTTP(w, req)
@@ -83,13 +83,13 @@ func Handler(routesHandler map[string]http.Handler) func(h http.Handler) http.Ha
 			res.Body.Close()
 
 			if len(b) == config.ContentTypeByteLimit+1 {
-				log.Info("Response exceeds acceptable byte limit for assessing content-type. Falling through to default handling", nil)
+				log.Event(req.Context(), "Response exceeds acceptable byte limit for assessing content-type. Falling through to default handling")
 				h.ServeHTTP(w, req)
 				return
 			}
 
 			if err != nil {
-				log.ErrorR(req, err, nil)
+				log.Event(req.Context(), "reading error", log.Error(err), nil)
 				h.ServeHTTP(w, req)
 				return
 			}
@@ -99,13 +99,12 @@ func Handler(routesHandler map[string]http.Handler) func(h http.Handler) http.Ha
 				DatasetID string `json:"apiDatasetId"`
 			}{}
 			if err := json.Unmarshal(b, &zebResp); err != nil {
-				log.ErrorR(req, err, nil)
+				log.Event(req.Context(), "json unmarshal error", log.Error(err))
 				h.ServeHTTP(w, req)
 				return
 			}
 
-			log.Debug(zebResp.Type, nil)
-			log.Debug(zebResp.DatasetID, nil)
+			log.Event(req.Context(), "zebedee response", log.Data{"type": zebResp.Type, "datasetID": zebResp.DatasetID})
 
 			pageType := res.Header.Get("ONS-Page-Type")
 
@@ -115,7 +114,7 @@ func Handler(routesHandler map[string]http.Handler) func(h http.Handler) http.Ha
 			}
 
 			if h, ok := routesHandler[pageType]; ok {
-				log.DebugR(req, "Using handler for page type", log.Data{"pageType": pageType, "url": contentURL})
+				log.Event(req.Context(), "Using handler for page type", log.Data{"pageType": pageType, "url": contentURL})
 				h.ServeHTTP(w, req)
 				return
 			}
