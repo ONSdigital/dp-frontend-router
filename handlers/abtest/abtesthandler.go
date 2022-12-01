ef0bf6181b75c24e76e3ad70220ae2c366e9c553
rahulmadathumpalliyalil <rahulmp.87@gmail.com>
2022-12-01 18:02:12 +0000

    Fix lint issues

--- handlers/abtest/abtesthandler.go
+++ handlers/abtest/abtesthandler.go
@@ -18,12 +18,11 @@ const (
 // Handler returns the relevant handler on the basis of the supplied parameters.
 // It delegates to both abTestHandler and abTestPurgeHandler on the basis the abTest parameter, but it is really
 // an encapsulation of the decision-making process as to what handler is used.
-func Handler(abTest bool, new, old http.Handler, percentage int, aspectID, domain, exitNew string) http.Handler {
+func Handler(abTest bool, newHandler, old http.Handler, percentage int, aspectID, domain, exitNew string) http.Handler {
 	if abTest {
-		return abTestHandler(new, old, percentage, aspectID, domain, exitNew)
-	} else {
-		return abTestPurgeHandler(new, aspectID, domain)
+		return abTestHandler(newHandler, old, percentage, aspectID, domain, exitNew)
 	}
+	return abTestPurgeHandler(newHandler, aspectID, domain)
 }
 
 // abTestHandler routes requests to either the old or new handler, for a given aspectID, according to the given percentage
@@ -33,7 +32,7 @@ func Handler(abTest bool, new, old http.Handler, percentage int, aspectID, domai
 // the DefaultABTestRandomiser in the library is sufficient
 // A well known string - the exitNew string -  can be used as a query parameter to the call, in order to definitively chose
 // the old handler
-func abTestHandler(new, old http.Handler, percentage int, aspectID, domain, exitNew string) http.Handler {
+func abTestHandler(newHandler, old http.Handler, percentage int, aspectID, domain, exitNew string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		now := time.Now().UTC()
 
@@ -45,11 +44,11 @@ func abTestHandler(new, old http.Handler, percentage int, aspectID, domain, exit
 		aspect := cookies.GetABTestCookieAspect(req, aspectID)
 
 		if (aspect.New.IsZero() && aspect.Old.IsZero()) || (aspect.New.Before(now) && aspect.Old.Before(now)) {
-			cookies.HandleCookieAndServ(w, req, new, old, aspectID, domain, cookies.DefaultABTestRandomiser(percentage))
+			cookies.HandleCookieAndServ(w, req, newHandler, old, aspectID, domain, cookies.DefaultABTestRandomiser(percentage))
 			return
 		}
 
-		cookies.ServABTest(w, req, new, old, aspect)
+		cookies.ServABTest(w, req, newHandler, old, aspect)
 	})
 }
 
@@ -57,9 +56,9 @@ func abTestHandler(new, old http.Handler, percentage int, aspectID, domain, exit
 // It is useful when AB Testing for a particular aspect has finished, but the aspect is still embedded in client's ab_test
 // cookie - this handler will remove the aspect and can be left in use for several weeks after testing has finished to 'clean'
 // the underlying ab_test cookie
-func abTestPurgeHandler(new http.Handler, aspectID, domain string) http.Handler {
+func abTestPurgeHandler(newHandler http.Handler, aspectID, domain string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		cookies.RemoveABTestCookieAspect(w, req, aspectID, domain)
-		new.ServeHTTP(w, req)
+		newHandler.ServeHTTP(w, req)
 	})
 }
