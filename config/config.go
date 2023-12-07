35b66be491dcf0ced4434b970e54e08db37dd907
Andrea K <andrea.kyurchiev@flaxandteal.co.uk>
2023-12-07 12:29:58 +0200

    adds RendererURL back to config struct
    Signed-off-by: lindenmckenzie <linden.mckenzie@ons.gov.uk>

--- config/config.go
+++ config/config.go
@@ -43,6 +43,7 @@ type Config struct {
 	RedirectSecret                  string        `envconfig:"REDIRECT_SECRET" json:"-"`
 	ReleaseCalendarControllerURL    string        `envconfig:"RELEASE_CALENDAR_CONTROLLER_URL"`
 	ReleaseCalendarEnabled          bool          `envconfig:"RELEASE_CALENDAR_ENABLED"`
+	RendererURL                     string        `envconfig:"RENDERER_URL"`
 	ReleaseCalendarRoutePrefix      string        `envconfig:"RELEASE_CALENDAR_ROUTE_PREFIX"`
 	ReleaseCalendarABTestPercentage int           `envconfig:"RELEASE_CALENDAR_AB_TEST_PERCENTAGE"`
 	SearchControllerURL             string        `envconfig:"SEARCH_CONTROLLER_URL"`
