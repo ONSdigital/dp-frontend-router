56d3f50e3fd306ecb15cd85e6d86d848ef10eb21
Dionisi Lola <dionisislolas@gmail.com>
2024-02-07 13:38:46 +0000

    Add unit test for release calendar handler

---
+++ handlers/relcal/relcalhandler_test.go
@@ -0,0 +1,76 @@
+package relcal
+
+import (
+	"github.com/ONSdigital/dp-frontend-router/config"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+const (
+	newServed = "New Served"
+	oldServed = "Old Served"
+)
+
+func TestRelcalHandler(t *testing.T) {
+	Convey("Given an old request handler and a new request handler", t, func() {
+		oldHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			_, _ = w.Write([]byte(oldServed))
+		})
+		newHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			_, _ = w.Write([]byte(newServed))
+		})
+
+		numberRequests := 10
+		cfg, _ := config.Get()
+
+		Convey("And the config  to use the new release calendar is set to false", func() {
+			cfg.UseNewReleaseCalendar = false
+
+			Convey("When the requests are made to the relcalHandler", func() {
+				handler := Handler(cfg.UseNewReleaseCalendar, newHandler, oldHandler)
+
+				nh := 0
+				for i := 0; i < numberRequests; i++ {
+					req := httptest.NewRequest("GET", "/", http.NoBody)
+					resp := httptest.NewRecorder()
+					handler.ServeHTTP(resp, req)
+					b, _ := io.ReadAll(resp.Result().Body)
+					if string(b) == oldServed {
+						nh++
+					}
+				}
+
+				Convey("All the requests should be to the old handler", func() {
+					So(nh, ShouldEqual, numberRequests)
+				})
+			})
+		})
+
+		Convey("And the config  to use the new release calendar is set to true", func() {
+			cfg.UseNewReleaseCalendar = true
+
+			Convey("When the requests are made to the relcalHandler", func() {
+				handler := Handler(cfg.UseNewReleaseCalendar, newHandler, oldHandler)
+
+				nh := 0
+				for i := 0; i < numberRequests; i++ {
+					req := httptest.NewRequest("GET", "/", http.NoBody)
+					resp := httptest.NewRecorder()
+					handler.ServeHTTP(resp, req)
+					b, _ := io.ReadAll(resp.Result().Body)
+					if string(b) == newServed {
+						nh++
+					}
+				}
+
+				Convey("All the requests should be to the new handler", func() {
+					So(nh, ShouldEqual, numberRequests)
+				})
+			})
+		})
+	})
+}
