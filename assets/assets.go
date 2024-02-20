242472fbe7e513ea08eda5bef734922ab2c53f63
Geraint Edwards <gedgey@proton.me>
2024-02-20 18:44:15 +0000

    remove comments

--- assets/assets.go
+++ assets/assets.go
@@ -1,8 +1,5 @@
 package assets
 
-// //go:generate go get github.com/jteeuwen/go-bindata/go-bindata
-// //go:generate go-bindata -o redirects.go -pkg assets redirects/...
-
 import (
 	_ "embed"
 	"errors"
