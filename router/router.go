368e200bd8a428e5473dd375bcd3b4cca2f43190
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-06-17 15:57:42 +0100

    Re-route /filters paths to frontend-dataset-controller

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -95,6 +96,12 @@ func New(cfg Config) http.Handler {
 	router.Handle("/cookies{uri:.*}", cfg.CookieHandler)
 	router.Handle("/datasets/{uri:.*}", cfg.DatasetHandler)
 	router.Handle("/filter-outputs/{uri:.*}", cfg.FilterHandler)
+	// router.Handle("/filters/{uri:.*}", cfg.DatasetHandler)
+	router.HandleFunc("/filters/{uri:.*}", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println("HHHHHHHHHHHHHHH router")
+		target := appConfig.DatasetControllerURL + req.URL.Path
+		http.Redirect(w, req, target, http.StatusTemporaryRedirect)
+	})
 	router.Handle("/feedback{uri:.*}", cfg.FeedbackHandler)
 
 	if cfg.LegacySearchRedirectsEnabled {
